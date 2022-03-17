c94a94bbfe6f9d45ff49ee250b84bb0338ec0cd1
CarlosJimeEnez <jjimenezpena7@gmail.com>
2022-03-16 22:30:23 -0600

    Interfaz azul

--- Código/Esp_Server_AP_WiFi (Qba)/include/index.h
+++ Código/Esp_Server_AP_WiFi (Qba)/include/index.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 String html = R"***(
-<html lang="en">
+    <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,24 +11,39 @@ String html = R"***(
             .body{
                 background-color: rgb(39, 41, 41);
             }
+            .superior{
+                border-radius: 0% 0% 15% 15% ;
+                height: 7vh;
+                background-color: #3b3b3b2a;
+                display: flex;
+                justify-content: center;
+            }
             .container{
                 background-color: rgb(39, 41, 41);
                 display: flex;
-                
+                justify-content: space-around;
             }
             .header{
                 background-color: rgb(39, 41, 41);
-
                 display: flex;
                 height: 30vh;
                 align-items: center;
                 justify-content: center;
             }
             .child{
-                background-color: #79018C;
+                background-color: #315ce7;
                 border-radius: 20%;
-                width: 25%;
+                width: 36%;
                 height: 12vh;
+                position: relative;
+            }
+
+            .child_flex{
+                background-color: #315ce7;
+                border-radius: 20%;
+                height: 20vh;
+                width: 25%;
+                margin-top: 20px;
                 position: relative;
             }
 
@@ -48,8 +63,42 @@ String html = R"***(
                 border-right-width: 0px;
             }
             
+            .play_righ{
+                background-color: rgba(132, 0, 255, 0);
+                border: 29px;
+                border-radius: 9%;
+                border-style: solid;
+                border-style: double;
+                border-color: rgba(255, 255, 255, 0) rgba(0, 0, 255, 0) rgba(255, 0, 0, 0) rgb(245, 245, 239);
+                height: 0;
+                
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                margin-top: -30px;
+                margin-left: -13px;
+                border-right-width: 0px;
+            }
+
+            .play_left{
+                background-color: rgba(132, 0, 255, 0);
+                border: 29px;
+                border-radius: 9%;
+                border-style: solid;
+                border-style: double;
+                border-color: rgba(255, 255, 255, 0) rgba(0, 0, 255, 0) rgba(255, 0, 0, 0) rgb(245, 245, 239);
+                height: 0;
+
+                position: absolute;
+                right: 50%;
+                top: 50%;
+                margin-right: -13px;
+                margin-top: -30px;
+                rotate: 180deg;
+            }
+
             .stop{
-                background-color: #79018C;
+                background-color: 5D8BF4;
                 border: 20px;
                 border-style: solid;
                 border-style: double;
@@ -62,14 +111,24 @@ String html = R"***(
                 margin-top: -25px;
                 margin-left: -19px;
             }
-    
+            
+            .background{
+                border-radius: 15% 15% 0 0 ;
+                margin-top: 55%;
+                height: 10vh;
+                background-color: #3b3b3b2a;
+                display: flex;
+                justify-content: center;
+            }
+
+
             input[type=range] {
                 height: 33px;
                 -webkit-appearance: none;
                 margin: 10px 0;
                 width: 100%;
                 }
-    
+                
             input[type=range]:focus {
             outline: none;
             }
@@ -78,7 +137,7 @@ String html = R"***(
             height: 13px;
             cursor: pointer;
             box-shadow: 0px 0px 0px #000000;
-            background: #B5164B;
+            background: #315ce7;
             border-radius: 25px;
             border: 0px solid #000101;
             }
@@ -94,14 +153,14 @@ String html = R"***(
             margin-top: -7px;
             }
             input[type=range]:focus::-webkit-slider-runnable-track {
-            background: #B5164B;
+            background: #315ce7;
             }
             input[type=range]::-moz-range-track {
             width: 100%;
             height: 13px;
             cursor: pointer;
             box-shadow: 0px 0px 0px #000000;
-            background: #B5164B;
+            background: #315ce7;
             border-radius: 25px;
             border: 0px solid #000101;
             }
@@ -123,13 +182,13 @@ String html = R"***(
             color: transparent;
             }
             input[type=range]::-ms-fill-lower {
-            background: #B5164B;
+            background: #315ce7;
             border: 0px solid #000101;
             border-radius: 50px;
             box-shadow: 0px 0px 0px #000000;
             }
             input[type=range]::-ms-fill-upper {
-            background: #B5164B;
+            background: #315ce7;
             border: 0px solid #000101;
             border-radius: 50px;
             box-shadow: 0px 0px 0px #000000;
@@ -145,10 +204,10 @@ String html = R"***(
             cursor: pointer;
             }
             input[type=range]:focus::-ms-fill-lower {
-            background: #B5164B;
+            background: #315ce7;
             }
             input[type=range]:focus::-ms-fill-upper {
-            background: #B5164B;
+            background: #315ce7;
             }
             
             #vel_range{
@@ -158,6 +217,7 @@ String html = R"***(
     
     </head>
     <body class="body">
+        <div class="superior">  </div>
         <div class="header">
             <div class="child"> 
                 <input type="button" class="play" onclick="play()">
@@ -165,8 +225,20 @@ String html = R"***(
         </div>
     
         <div class="container">
-            <input type="range" name="" id="vel_range" max="99">
+            <input type="range" name="" id="vel_range">
         </div>
+
+        <div class="container">
+            <div class="child_flex">
+                <input type="button" class="play_left" value="">
+            </div>
+            <div class="child_flex">
+               <input type="button"  class="play_righ" value="">
+            </div>
+        </div>
+
+        <div class="background">  </div>
+       
     </body>
     
     
@@ -212,5 +284,4 @@ String html = R"***(
         }
     </script>
     </html>
-
     )***";
